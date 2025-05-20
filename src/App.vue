f1381190b4a8445f40e9e5b64e6ab8f9ecf7f4f3
Adrizen89 <adrienberard@hotmail.fr>
2025-05-20 17:21:57 +0200

    adapt app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <script setup>
 import Sidebar from "./core/layouts/Sidebar.vue";
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 </script>
 
 <template>
   <div class="flex">
+    <!-- Ne montre la sidebar que si showSidebar est vrai ou non défini -->
     <Sidebar v-if="route.meta.showSidebar !== false" />
+    
     <div class="main-content">
       <router-view />
     </div>
-    
-    </div>
+  </div>
 </template>
 
 <style scoped>
-  .main-content {
-    flex: 1;
+.main-content {
+  flex: 1;
   padding: 1rem;
   overflow-x: hidden;
   transition: margin-left 0.3s ease;
-  }
-  
+}
 </style>
