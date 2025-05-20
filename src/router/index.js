5f1cd6d29b5ad9ef888acd9d0ebc3d80b6f836be
Adrizen89 <adrienberard@hotmail.fr>
2025-05-20 14:42:37 +0200

    cors

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,29 @@ import CreateFiche from "../modules/fiches/views/CreateFiche.vue";
 import FicheInProgress from "../modules/fiches/views/FicheInProgress.vue";
 import HistoriqueFiche from "../modules/fiches/views/HistoriqueFiche.vue";
 import FicheDetails from "../modules/fiches/components/FicheDetails.vue";
+import NotFound from "../core/layouts/NotFound.vue";
 
 
 const routes = [
-  { path: "/", component: Home },
+  { path: "/fbfichecontact", name: "PrivateHome", component: Home },
+
   { path: "/fiche-in-progress", component: FicheInProgress },
   { path: "/create-fiche", component: CreateFiche },
-  { path: "/fiche-in-progress/:id", name: "ficheInProgressDetails",component: FicheInProgressDetails},
-  { path: "/fiche/:id", name: "ficheDetails",component: FicheDetails},
-  { path: "/historique", component: HistoriqueFiche},
+  { path: "/fiche-in-progress/:id", name: "ficheInProgressDetails", component: FicheInProgressDetails },
+  { path: "/fiche/:id", name: "ficheDetails", component: FicheDetails },
+  { path: "/historique", component: HistoriqueFiche },
 
-  
+  // 🔁 Redirection de "/" vers ta page cachée
+  { path: "/", redirect: "/fbfichecontact" },
+
+  // 🚫 404 pour tout le reste
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+  }
 ];
+
 
 const router = createRouter({
   history: createWebHistory(),
